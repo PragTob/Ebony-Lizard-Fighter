607aab47138da66f5d73cd185057ea7459c93580
Simon <simonh@mindcandy.com>
2012-11-10 13:12:00 +0000

    Changed sword firing to somethings very strange

--- js/game.js
+++ js/game.js
@@ -66,6 +66,13 @@
                     this.unflip()
                 }
             });
+            Crafty("StupidFuckingRectangle").bind('Click', this.emitSword);
+        },
+
+        emitSword: function()
+        {
+            console.log("attempting to fire sword");
+            Crafty.e('Sword').attr({x: Crafty('Player').x, y: Crafty('Player').y});
         }
     });
 
@@ -115,6 +122,7 @@
         function onLoaded() {
             // set up sprites
             Crafty.sprite(60, 'img/LizardSpriteSheet.png', { lizard: [0, 0] });
+            Crafty.sprite(60, 'img/Sword.png', { sword: [0, 0, 13,32] });
 
             // jump to the main scene in half a second
             loading.delay(function () {
@@ -132,17 +140,26 @@
 
         // list of images to load
         Crafty.load(
-        ['img/LizardSpriteSheet.png'],
+        ['img/LizardSpriteSheet.png', 'img/sword.png'],
         onLoaded, onProgress, onError);
 
     };
+
+    Crafty.c('StupidFuckingRectangle', {
+        init: function() {
+            this.requires('2D,DOM,Mouse')
+                .attr({x:0,y:0,w: 1000, h: 600,z:99999999});
+        }
+    });
 
     //
     // The main game scene
     //
     Game.prototype.mainScene = function () {
         //create a player...
+        Crafty.e('StupidFuckingRectangle');
         Crafty.e('Player');
+
 
         //This is the floor
         Crafty.e('Platform').attr({x: 0, y: 584, w: 1000, h: 16});
@@ -152,6 +169,57 @@
         Crafty.e('Platform').attr({x: 500, y: 300, w: 100, h: 16});
         Crafty.e('Platform').attr({x: 700, y: 450, w: 100, h: 16});
     };
+
+    Crafty.c('Sword', {
+        init:function()
+        {
+            console.log("sword firing");
+            var player = Crafty('Player');
+            this.requires('Renderable, Collision, ViewportBounded')
+            .spriteName('sword')
+            .attr({w:13,h:32})
+            .collision();
+            this.yspeed = 1;
+            this.xspeed = 1;
+
+            var mousey = 3;
+            var mousex = 3;
+
+
+
+//            var radius = Math.sqrt(Math.pow(Crafty('Player').x - mousex, 2) + Math.pow(Crafty('Player').y - mousey, 2));
+
+            var angleRadians = Math.atan(((Crafty('Player').y - mousey) / (Crafty('Player').x - mousex)));
+            this.xspeed = Math.sin(angleRadians * (Math.PI/180)) * 10;
+            this.yspeed = Math.cos(angleRadians / (Math.PI/180)) * 10;
+
+            console.log("Blah" + this.xspeed + this.yspeed);
+            
+            this.bind('EnterFrame',this.updateMovement);
+            this.bind('EnterFrame',this.checkDead);
+            player.z = this.z+1;
+        },
+
+        updateMovement : function()
+        {
+//            this.rotate(10);
+            this.x+=this.xspeed;
+            this.y+=this.yspeed;
+        },
+
+        checkDead : function()
+        {
+            if(this.x>(Crafty.viewport.width + 58))
+            {
+                this.destroy();
+            }
+
+            if(this.x < 0)
+            {
+                this.destroy();
+            }
+        }
+    });
 
     // kick off the game when the web page is ready
     $(document).ready(function () {
